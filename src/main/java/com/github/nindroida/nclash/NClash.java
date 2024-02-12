668a3ddcd458963808f06c44e3761c2f171f2b38
NindroidA <andrew@larrycurtis.com>
2024-02-12 17:07:16 -0600

    Initial commit

---
+++ src/main/java/com/github/nindroida/nclash/NClash.java
@@ -0,0 +1,17 @@
+package com.github.nindroida.nclash;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NClash extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
